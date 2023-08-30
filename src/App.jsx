c6dd4274df2f38328d436258c310a774dda1d867
Imtinan <91307080+ImtinanFakhar@users.noreply.github.com>
2023-08-31 01:31:21 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ const App = () => {
       <div>
         <Navigation />
         <Routes>
-          <Route path="/" element={<Home />}  /> 
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home/>}  /> 
+          <Route path="/home" element={<Home/>}  /> 
+          <Route path="/about" element={<About/>} />
           <Route path="/contact" element={<Contact/>} />
           
            </Routes>
